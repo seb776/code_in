38a3d690d0c4c8eb5cf2e0e0170d38aa379868da
seb776 <sebastien.maire@epitech.eu>
2015-05-13 20:52:40 +0200

    Modified script so we can just browse with a folder view to the folder of devenv.exe, it now creates a file and stores the result. You will just have to configure it once.

--- configure.bat
+++ configure.bat
@@ -1,15 +1,37 @@
-set pathDevEnv=C:\Program Files %%28x86%%29\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe
-set workingDir=code_in_test
-set configFile=./code_in_test/code_in_test.csproj.user
-SET CURRENTDIR=%cd%
+:: Author Sebastien Maire
+:: Contribs
 
 @echo off
+
+set pathDevEnv=
+set pathDevEnvFilePath=pathDevEnv.cfg
+set workingDir=code_in_test
+set configFile=./code_in_test/code_in_test.csproj.user
+set CURRENTDIR=%cd%
+
+:setPath
+if exist %pathDevEnvFilePath% (
+echo Found %pathDevEnvFilePath% that stores devenv.exe path
+) else (
+echo configure.bat did not found %pathDevEnvFilePath% and cannot configure.
+echo Please give the path to the folder that contains devenv.exe  generally "X:/.../Microsoft Visual Studio xx.0/Common7/IDE/"
+folderBrowser.exe > %pathDevEnvFilePath%
+)
+
+set /p pathDevEnv=< %pathDevEnvFilePath%
+
+if not exist "%pathDevEnv%\devenv.exe" (
+echo  "%pathDevEnv%\devenv.exe"
+echo Wrong path, you must give a valid path to devenv.exe
+del %pathDevEnvFilePath%
+goto :setPath
+)
 
 echo ^<^?xml version="1.0" encoding="utf-8"?^> > %configFile%
 echo ^<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> %configFile%
 echo   ^<PropertyGroup Condition="'$(Configuration)|$(Platform)' == 'Debug|AnyCPU'"^> >> %configFile%
 echo     ^<StartAction^>Program^</StartAction^> >> %configFile%
-echo     ^<StartProgram^>%pathDevEnv%^</StartProgram^> >> %configFile%
+echo     ^<StartProgram^>%pathDevEnv%\devenv.exe^</StartProgram^> >> %configFile%
 echo     ^<StartArguments^>/resetaddin code_in_test.Connect^</StartArguments^> >> %configFile%
 echo     ^<StartWorkingDirectory^>%CURRENTDIR%\%workingDir%^</StartWorkingDirectory^> >> %configFile%
 echo   ^</PropertyGroup^> >> %configFile%
