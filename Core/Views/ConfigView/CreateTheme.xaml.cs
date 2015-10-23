bc9c19665898820ceac3f3786e9a223e4005e62e
bloubtek <morgane.ferrandis@epitech.eu>
2015-10-23 17:39:53 +0200

    modification of the UC "Create theme", Setting of the name, combobox for choosing an existing theme as template or suppressing it (did a list of themes that i display on the combobox, disapaer on it when suppressed), the buttons import/export (that do nothing for now), preview of the theme on the right

--- Core/Views/ConfigView/CreateTheme.xaml.cs
+++ Core/Views/ConfigView/CreateTheme.xaml.cs
@@ -20,9 +20,50 @@ namespace code_in.Views.ConfigView
     /// </summary>
     public partial class CreateTheme : UserControl
     {
+        private List<string> themeList = new List<string>();
+        private string name = "";
         public CreateTheme()
         {
+            int i = 0;
             InitializeComponent();
+            themeList.Add("DarkTheme");
+            themeList.Add("LightTheme");
+            while (i != themeList.Count)
+            {
+                BoxTheme.Items.Add(themeList[i]);
+                ++i;
+            }
+            ThemeName.AddHandler(TextBox.TextInputEvent,
+                   new TextCompositionEventHandler(ThemeName_TextInput_1),
+                   true);
         }
+
+        private void SupprTheme(object sender, RoutedEventArgs e)
+        {
+            if (BoxTheme.SelectedItem.ToString() != null)
+            {
+                themeList.Remove(BoxTheme.SelectedItem.ToString());
+                BoxTheme.Items.Remove(BoxTheme.SelectedItem);
+            }
+        }
+
+        private void ThemeName_TextInput_1(object sender, TextCompositionEventArgs e)
+        {
+            ThemeName.KeyUp += new KeyEventHandler(ThemeName_KeyDown);
+
+            if (e.Text != "\n")
+            {
+                name += e.Text;
+            }
+        }
+
+        private void ThemeName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                PreviewName.Text = "Aperçu de " + name;
+            }
+        }
+
     }
 }
