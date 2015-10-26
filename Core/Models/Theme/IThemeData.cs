424905b73a775475671df46214b7cb71b29a2021
roy_s <severine.roy@epitech.eu>
2015-10-26 14:53:34 +0000

    First commit of IThemeData and DefaultThemeData

---
+++ Core/Models/Theme/IThemeData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace code_in.Models.Theme
+{
+    public interface IThemeData
+    {
+       SolidColorBrush getNodeForegroundColor();
+       SolidColorBrush getNodeBackgroundColor();
+       SolidColorBrush getNodeTitleColor();
+       SolidColorBrush getNodeItemColor();
+   //    int getIntColor();
+        /*
+getdouble/float/short/char/bool/enum/class..color
+getDeclColor
+getFuncDecl
+getGridBackground/foreground/thickness/dottedDistance/xSize/ySize
+getLinkDrawer return ILinkDraw*/
+
+    }
+}
