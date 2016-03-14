8374cc59810207eaf5dc601485dd73b0092f5a91
seb776 <sebastien.maire@epitech.eu>
2016-03-14 20:29:10 +0000

    Added VSCode_in as VSPackage. Need to remove the old VSadd_in.

---
+++ VSCode_in/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VSCode_in")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Code_in")]
+[assembly: AssemblyProduct("VSCode_in")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("VSCode_in_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005f7cb4fa426d30268fd8d64c7e6fca4b64d43b101a92bb9330386abc63f8586ba3249d82b34d3c35391395c2acc15a83bec15495a232dcbaae365f5631550cc016c0e42652d692e99f7a5005665315ed3516fb17e43199c5c6df5f4044b0d7d5df0a9e1489240b73ad37c39fb4a3df6ec34d97e5dc33147eeb9ffaf20cbc33b5")]
+[assembly: InternalsVisibleTo("VSCode_in_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005f7cb4fa426d30268fd8d64c7e6fca4b64d43b101a92bb9330386abc63f8586ba3249d82b34d3c35391395c2acc15a83bec15495a232dcbaae365f5631550cc016c0e42652d692e99f7a5005665315ed3516fb17e43199c5c6df5f4044b0d7d5df0a9e1489240b73ad37c39fb4a3df6ec34d97e5dc33147eeb9ffaf20cbc33b5")]
---
+++ VSCode_in/VSCode_inPackage.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace Code_in.VSCode_in
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    ///
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the 
+    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// register itself and its components with the shell.
+    /// </summary>
+    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
+    // a package.
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    // This attribute is used to register the information needed to show this package
+    // in the Help/About dialog of Visual Studio.
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    // This attribute is needed to let the shell know that this package exposes some menus.
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(ConfigWindowPane), Style = VsDockStyle.MDI, Window = "3ae79031-e1bc-11d0-8f78-00a0c9110058", Transient = true)] // TODO Wrong GUID ?
+    [ProvideToolWindow(typeof(NodalWindowPane), Style = VsDockStyle.MDI, Window = "3ae79031-e1bc-11d0-8f78-00a0c9110058", Transient = true)] // TODO Wrong GUID ?
+    [Guid(GuidList.guidVSCode_inPkgString)]
+    public sealed class VSCode_inPackage : Package
+    {
+        /// <summary>
+        /// Default constructor of the package.
+        /// Inside this method you can place any initialization code that does not require 
+        /// any Visual Studio service because at this point the package object is created but 
+        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// initialization is the Initialize method.
+        /// </summary>
+        public VSCode_inPackage()
+        {
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
+        }
+
+
+
+        /////////////////////////////////////////////////////////////////////////////
+        // Overridden Package Implementation
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+            base.Initialize();
+
+            // Add our command handlers for menu (commands must exist in the .vsct file)
+            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null != mcs)
+            {
+                // Create the command for the menu item.
+                CommandID openFileMenuCommandID = new CommandID(GuidList.guidVSCode_inCmdSet, (int)PkgCmdIDList.cmdidOpenFile);
+                MenuCommand openFileMenuItem = new MenuCommand(OpenFileCallback, openFileMenuCommandID);
+                mcs.AddCommand(openFileMenuItem);
+
+                CommandID configMenuCommandID = new CommandID(GuidList.guidVSCode_inCmdSet, (int)PkgCmdIDList.cmdidConfigMenu);
+                MenuCommand configMenuItem = new MenuCommand(ConfigCallback, configMenuCommandID);
+                mcs.AddCommand(configMenuItem);
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// This function is the callback used to execute a command when the a menu item is clicked.
+        /// See the Initialize method to see how the menu item is associated to this function using
+        /// the OleMenuCommandService service and the MenuCommand class.
+        /// </summary>
+        private void OpenFileCallback(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            while (this.FindToolWindow(typeof(NodalWindowPane), i, false) != null)
+                ++i;
+            
+            ToolWindowPane wp = this.CreateToolWindow(typeof(NodalWindowPane), i) as ToolWindowPane;
+
+            if (wp != null)
+            {
+                IVsWindowFrame frame = wp.Frame as IVsWindowFrame;
+                if (frame != null)
+                {
+                    frame.SetProperty((int)Microsoft.VisualStudio.Shell.Interop.__VSFPROPID.VSFPROPID_FrameMode, VSFRAMEMODE.VSFM_MdiChild);
+                    frame.Show();
+                }
+            }
+        }
+
+        private void ConfigCallback(object sender, EventArgs e)
+        {
+            ToolWindowPane wp = this.FindToolWindow(typeof(ConfigWindowPane), 0, true);
+
+            if (wp != null)
+            {
+                IVsWindowFrame frame = wp.Frame as IVsWindowFrame;
+                if (frame != null)
+                {
+                    frame.SetProperty((int)Microsoft.VisualStudio.Shell.Interop.__VSFPROPID.VSFPROPID_FrameMode, VSFRAMEMODE.VSFM_MdiChild);
+                    frame.Show();
+                }
+            }
+
+
+        }
+    }
+
+    public class ConfigWindowPane : ToolWindowPane // TODO move this elsewhere
+    {
+        private code_in.Views.ConfigView.ConfigView _configView = null;
+        public ConfigWindowPane()
+        {
+            _configView =  new code_in.Views.ConfigView.ConfigView();
+            this.Content = _configView;
+        }
+    }
+
+    public class NodalWindowPane : ToolWindowPane
+    {
+        private code_in.Views.MainView.MainView _mainView = null;
+
+        public NodalWindowPane()
+        {
+            _mainView = new code_in.Views.MainView.MainView();
+            this.Content = _mainView;
+        }
+    }
+}
