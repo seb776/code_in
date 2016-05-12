7e9649e863aefc9d66983cc82ab382737f395725
Sebastien Maire <sebastien.maire@epitech.eu>
2016-05-12 12:17:33 +0100

    Added INodePresenter.cs to abstract NodePresenter behavior.

---
+++ Core/Presenters/Nodal/Nodes/INodePresenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// z0rg: This file is a work in progress
+// Just exposing few ideas to see how to use them
+
+namespace code_in.Presenters.Nodal.Nodes
+{
+    enum ENodeActions
+    {
+        MODIFIERS = 1,
+        NAME = 2,
+        GENERICS = 4,
+        ATTRIBUTE = 8,
+        INHERITANCE = 16,
+        CONSTRAINT = 32 // where T : ...
+    }
+    public interface INodePresenter
+    {
+        ENodeActions GetActions(); // Gets the possible actions for this node
+        void AddGeneric(String name);
+        void RemoveGeneric(int index);
+        void AddInheritance(String name);
+        //bool AddConstraint() // TODO @z0rg
+        void SetName(String name);
+    }
+}
