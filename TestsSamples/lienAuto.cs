b8a57d9242075c5757cd4640d13aef260aa82eb2
march <stephane1.nguyen@epitech.eu>
2016-06-20 11:18:55 +0200

    adding flow node auto link on entry/exit/single statement

---
+++ TestsSamples/lienAuto.cs
@@ -0,0 +1,53 @@
+﻿class LienAuto
+{
+
+    int returnBiggest(int x, int y)
+    {
+        int valRet;
+
+        if (x < y) 
+	{
+	}
+        else
+	{
+            
+	}
+	valret = valret + 42;
+        return valRet;
+    }
+
+    int returnParamA(int a)
+    {
+        return a;
+    }
+
+    int returnDeclParam()
+    {
+        int a;
+
+        a = 42;
+        return a;
+    }
+
+    void multipleDeclParam()
+    {
+	int a;
+	int b;
+	char c, d, e;
+    }
+
+    void loopWhile() 
+    {
+	int a;
+	
+	a = 0;
+	while (a < 42) 
+	{
+	}
+    }
+
+    void ifStmt(char c) {
+	if (c == 'c')	    
+            Console.WriteLine("Test");
+    }
+}
