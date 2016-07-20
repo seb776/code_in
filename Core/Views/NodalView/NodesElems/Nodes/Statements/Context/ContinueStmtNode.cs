a513a1e5937d1219ac1b6a0bb9f6c96d82dab425
Yassine Lamant <yassine.eip@gmail.com>
2016-07-20 16:09:38 +0200

    add ContinueStmtNode.cs

---
+++ Core/Views/NodalView/NodesElems/Nodes/Statements/Context/ContinueStmtNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using code_in.Views.NodalView.NodesElems.Items;
+using code_in.Views.NodalView.NodesElems.Nodes.Statements.Base;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace code_in.Views.NodalView.NodesElems.Nodes.Statements.Context
+{
+    class ContinueStmtNode : AContextStmtNode
+    {
+        public ContinueStmtNode(ResourceDictionary themeResDict) :
+            base(themeResDict)
+        {
+            this.CreateAndAddInput<FlowNodeItem>();
+        }
+    }
+}
