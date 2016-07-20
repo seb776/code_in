775eec9e0215662ffca113bc52f195d1c3cc4a30
Yassine Lamant <yassine.eip@gmail.com>
2016-07-20 17:03:26 +0200

    add visual node for continue and value

---
+++ Core/Views/NodalView/NodesElems/Nodes/Expressions/ValueExprNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace code_in.Views.NodalView.NodesElems.Nodes.Expressions
+{
+    class ValueExprNode : AValueNode
+    {
+        public ValueExprNode(ResourceDictionary themeResDict)
+            : base(themeResDict)
+        { 
+        
+        }
+    }
+}
---
+++ Core/Views/NodalView/NodesElems/Nodes/Statements/Context/ContinueStmtNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using code_in.Views.NodalView.NodesElems.Anchors;
+using code_in.Views.NodalView.NodesElems.Items;
+using code_in.Views.NodalView.NodesElems.Nodes.Statements.Base;
+
+namespace code_in.Views.NodalView.NodesElems.Nodes.Statements.Context
+{
+    class ContinueStmtNode : AContextStmtNode
+    {
+        public ContinueStmtNode(ResourceDictionary themeResDict) :
+            base(themeResDict)
+        {
+        }
+    }
+}
