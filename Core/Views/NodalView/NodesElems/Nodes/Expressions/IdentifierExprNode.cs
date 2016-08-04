8615917b1b6089e168e5506650ae2269b2123934
Sebastien Maire <sebastien.maire@epitech.eu>
2016-08-05 00:38:59 +0200

    Added support for ArrayInitializersExpr. Added support for ArrayCreateExpr. Created node for PrimaryExprNode.cs, IdentifierExprNode.cs, ArrayCreateExprNode.cs, ArrayInitExprNode.cs

---
+++ Core/Views/NodalView/NodesElems/Nodes/Expressions/IdentifierExprNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace code_in.Views.NodalView.NodesElems.Nodes.Expressions
+{
+    public class IdentifierExprNode : AValueNode
+    {
+        public IdentifierExprNode(ResourceDictionary themeResDict) :
+            base(themeResDict)
+        {
+            this.SetType("IdentifierExpr");
+        }
+
+        public override void InstantiateASTNode() // TODO
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Views/NodalView/NodesElems/Nodes/Expressions/PrimaryExprNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace code_in.Views.NodalView.NodesElems.Nodes.Expressions
+{
+    public class PrimaryExprNode : AValueNode
+    {
+        public PrimaryExprNode(ResourceDictionary themeResDict) :
+            base(themeResDict)
+        {
+            this.SetType("PrimaryExpr");
+        }
+
+        public override void InstantiateASTNode() // TODO
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
