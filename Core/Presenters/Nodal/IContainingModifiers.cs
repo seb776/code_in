8b7636416bfa4d0249d59e566ab93e584124201d
BloubTek <morgane.ferrandis@epitech.eu>
2016-08-12 16:56:09 +0200

    Created two interface to handle types and attributes from the node presenter. We can now edit type of vars and methods in ast and visual nodes.

--- Core/Presenters/Nodal/IContainingModifiers.cs
+++ Core/Presenters/Nodal/IContainingModifiers.cs
@@ -10,37 +10,5 @@ namespace code_in.Presenters.Nodal
     interface IContainingModifiers
     {
         void setModifiersList(Modifiers modifiers); // TODO @Mo the view should not be aware of NRefactory types
-/*        {
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.New) == ICSharpCode.NRefactory.CSharp.Modifiers.New)
-                ModifiersList.Add("new");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Partial) == ICSharpCode.NRefactory.CSharp.Modifiers.Partial)
-                ModifiersList.Add("partial");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Static) == ICSharpCode.NRefactory.CSharp.Modifiers.Static)
-                ModifiersList.Add("static");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Abstract) == ICSharpCode.NRefactory.CSharp.Modifiers.Abstract)
-                ModifiersList.Add("abstract");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Const) == ICSharpCode.NRefactory.CSharp.Modifiers.Const)
-                ModifiersList.Add("Const");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Async) == ICSharpCode.NRefactory.CSharp.Modifiers.Async)
-                ModifiersList.Add("async");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Override) == ICSharpCode.NRefactory.CSharp.Modifiers.Override)
-                ModifiersList.Add("override");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Virtual) == ICSharpCode.NRefactory.CSharp.Modifiers.Virtual)
-                ModifiersList.Add("virtual");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Extern) == ICSharpCode.NRefactory.CSharp.Modifiers.Extern)
-                ModifiersList.Add("extern");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Override) == ICSharpCode.NRefactory.CSharp.Modifiers.Override)
-                ModifiersList.Add("override");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Readonly) == ICSharpCode.NRefactory.CSharp.Modifiers.Readonly)
-                ModifiersList.Add("readonly");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Sealed) == ICSharpCode.NRefactory.CSharp.Modifiers.Sealed)
-                ModifiersList.Add("sealed");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Unsafe) == ICSharpCode.NRefactory.CSharp.Modifiers.Unsafe)
-                ModifiersList.Add("unsafe");
-            if ((modifiers & ICSharpCode.NRefactory.CSharp.Modifiers.Volatile) == ICSharpCode.NRefactory.CSharp.Modifiers.Volatile)
-                ModifiersList.Add("volatile");
-
-        }*/
-
     }
 }
