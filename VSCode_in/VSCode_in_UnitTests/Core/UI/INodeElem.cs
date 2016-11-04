69cbde5f90e55541a6e5ce28485bdf2875f5260d
Sebastien Maire <sebastien.maire@epitech.eu>
2016-11-04 16:59:27 +0100

    Added few test examples for stephane.

---
+++ VSCode_in/VSCode_in_UnitTests/Core/UI/INodeElem.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSCode_in_UnitTests.Core.UI
+{
+    /// <summary>
+    /// TODO @Steph, check if the functions 
+    /// </summary>
+    [TestClass()]
+    public class INodeElemImplem
+    {
+        [TestMethod()]
+        void classDeclNode()
+        {
+            //new MainView()
+            //var classDeclNode = new code_in.Views.NodalView.NodesElems.Nodes.ClassDeclNode(code_in.Code_inApplication.MainResourceDictionary);
+            //try
+            //{
+            //    classDeclNode.UpdateDisplayedInfosFromPresenter();
+            //}
+            //catch (NotImplementedException e)
+            //{
+            //    throw new Exception();
+            //}
+            //classDeclNode.SetParentView()
+        }
+    }
+}
