ab5990fe7aeaebc2ef2b726d91d29f2ce186899a
Stéphane <stephane1.nguyen@epitech.eu>
2017-01-16 00:27:33 +0100

    FileSystemWatcher to detect when a file changed

--- Core/Views/NodalView/DeclarationsNodalView.cs
+++ Core/Views/NodalView/DeclarationsNodalView.cs
@@ -3,10 +3,12 @@ using code_in.Views.NodalView.NodesElems;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Security.Permissions;
 
 namespace code_in.Views.NodalView
 {
@@ -56,6 +58,30 @@ namespace code_in.Views.NodalView
             // TODO Show Animation loadingFile
             this.NodalPresenterDecl.OpenFile(path);
             AlignDeclarations();
+            FileSystemWatcher w = new FileSystemWatcher();
+            
+            if (this.NodalPresenterDecl._model != null)
+            {
+                var pathFile = this.NodalPresenterDecl._model.FilePath;
+
+                w.Path = Path.GetDirectoryName(pathFile);
+                // @ Zor (Hamham) : je t'ai mis juste le filtre quand la taille du fichier change (https://msdn.microsoft.com/fr-fr/library/system.io.notifyfilters(v=vs.110).aspx)
+                /* petit truc tet un peu dérangeant, je ne sais pas si il y a un paramètre ou autre mais quand le fichier est déjà ouvert 
+                 * avant que tu ne l'ouvres dans Code_in, si tu fais des changement, le callback ne sera pas appelé
+                 * Tu dois obligatoirement avoir ouvert le fichier dans code_in puis l'ouvrir à l'extérieur et faire le changement pour que cela marche
+                 */
+                w.NotifyFilter = NotifyFilters.Size;
+                w.Filter = Path.GetFileName(pathFile);
+                w.Changed += OnChanged;
+
+                w.EnableRaisingEvents = true;
+            }
+            
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            MessageBox.Show("changed");
         }
     }
 }
